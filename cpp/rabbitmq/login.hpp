5e6efe17f52a66da36994d1c6ae28e1280e0bc4f
Pablo Hoch <pablo@melaxis.com>
2022-03-02 12:17:15 +0100

    c++ wrapper: remove unused login options (exchange + routing_key)

--- cpp/rabbitmq/login.hpp
+++ cpp/rabbitmq/login.hpp
@@ -14,8 +14,7 @@ struct login {
     if (empty()) {
       return true;
     }
-    return !host_.empty() && port_ != 0U && !exchange_.empty() &&
-           !vhost_.empty() && !routing_key_.empty() && !user_.empty() &&
+    return !host_.empty() && port_ != 0U && !vhost_.empty() && !user_.empty() &&
            !pw_.empty() &&
            ((cert_.empty() && key_.empty()) ||
             (!cert_.empty() && !key_.empty()));
@@ -23,7 +22,7 @@ struct login {
 
   std::string host_;
   unsigned port_{0U};
-  std::string exchange_{"amq.direct"}, vhost_{"ribasis"}, routing_key_, queue_;
+  std::string vhost_{"ribasis"}, queue_;
   std::string user_, pw_;
   std::string ca_{"cacert.pem"}, cert_{"cert.pem"}, key_{"key.pem"};
   unsigned timeout_{10U};
